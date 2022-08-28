d1927b5f3e102daa0bebcb277ef984e4aa820b18
vantiem <112336823+vantiem@users.noreply.github.com>
2022-08-28 21:27:43 +0300

    Update 1proxy.sh

--- 1proxy.sh
+++ 1proxy.sh
@@ -16,11 +16,9 @@ install_3proxy() {
     URL="https://github.com/3proxy/3proxy/archive/refs/tags/0.8.13.tar.gz"
     wget -qO- $URL | bsdtar -xvf-
     cd 3proxy-3proxy-0.8.13
-    make -f Makefile.Linux
-    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
-    cp src/3proxy /usr/local/etc/3proxy/bin/
-    cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
-    chmod +x /etc/init.d/3proxy
+    ln -s Makefile.Linux Makefile
+    make
+    sudo make install
     systemctl 3proxy on
     echo "* hard nofile 999999" >>  /etc/security/limits.conf
     echo "* soft nofile 999999" >>  /etc/security/limits.conf
